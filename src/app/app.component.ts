ee6c64e2065e41f69b0ce0bfb2744328f740693a
eliyagalis <eliyagalisbiz@gmail.com>
2024-12-09 17:44:50 +0200

    create tasks angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
